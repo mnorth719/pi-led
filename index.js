47e0524f9f545827f6337140709dcdadd1290671
Kevin Aung <kevin.aung@yahoo.com>
2015-04-16 22:32:12 -0700

    http server w/ j5 and raspi

---
+++ index.js
@@ -0,0 +1,46 @@
+var http = require('http'),
+    raspi = require('raspi-io'),
+    five = require('johnny-five'),
+    board = five.Board({
+        io: new raspi()
+    });
+
+var port = 3000;
+
+board.on('ready', function() {
+    console.log('board is ready!');
+    
+    http.createServer(function(req, res) {
+        req.on('data', function(chunk) {
+            var data = JSON.parse(chunk.toString());
+            var led = new five.Led(data.pin);
+
+            console.log('executing command: "%s"', data.command);
+
+            switch (data.command) {
+                case 'on':
+                    led.on();
+                    break;
+
+                case 'off':
+                    led.off();
+                    break;
+
+                case 'strobe':
+                    led.strobe();
+                    break;
+
+                case 'stop':
+                    led.stop();
+                    break;
+
+                default:
+                    console.log('Invalid command. Available commands: "on", "off", "strobe", "stop"');
+                    break;
+            }
+        });
+    }).listen(3000);
+    console.log('listening on port ' + port);
+});
+
+console.log('preparing board...');
