64d61d4d032c9f77e3c613a4f64c1e57ef4febb3
Matt North <matt.north@cbsnorthstar.com>
2015-04-18 16:07:07 -0700

    forgot res.end

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ board.on('ready', function() {
                     if (data.ms) {
                         led.fadeIn(data.ms); 
                     }else{
-                        return res.writeHead(400, "Bad Request"); 
+                        return res.end(400, "Bad Request"); 
                     }
                 }
                     break;
@@ -52,7 +52,7 @@ board.on('ready', function() {
                     if (data.ms) {
                         led.fadeOut(data.ms); 
                     }else{
-                        return res.writeHead(400, "Bad Request"); 
+                        return res.end(400, "Bad Request"); 
 
                     }
                 }
